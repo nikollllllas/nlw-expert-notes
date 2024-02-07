ebd52f38d2986e36a1cbae31c2f47e9d3b069df8
nikollllllas <nikollasohta2014@gmail.com>
2024-02-06 22:55:43 -0300

    feat: updated NewNoteCard and NoteCard

--- src/App.tsx
+++ src/App.tsx
@@ -19,11 +19,15 @@ export function App() {
       <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
         <NewNoteCard />
 
-        <NoteCard />
-        
-        <NoteCard />
-        
-        <NoteCard />
+        <NoteCard note={{
+          date: new Date(),
+          content: 'Hello world'
+        }}/>
+
+        <NoteCard note={{
+          date: new Date(),
+          content: 'Hello world'
+        }}/>
         
       </div>
     </div>
